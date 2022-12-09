0a9c362a96e8f4525086ba7d3c0540bf5d7e77ee
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-08 21:54:47 -0700

    Ticks formed

--- server/engine/engine.js
+++ server/engine/engine.js
@@ -1,5 +1,36 @@
+const timeBetweenTicks = 5000; 
+const Tick = require('./game/tick');
+let timeToLastTick;
+
+async function doGameTick(){
+    console.log('  -EN> Starting Tick:');
+    const startOfTick = new Date();
+    Tick.doTick();
+    const endOfTick = new Date();
+    console.log(`  -EN> Game Tick took: ${endOfTick - startOfTick}ms :: Date: ${Tick.getDate}`);
+}
+
+async function startLoop(){
+    const currentTick = new Date();
+    
+    // console.log('  -> Time Sense Last Tick:',currentTick - timeToLastTick);
+    if((currentTick - timeToLastTick) > timeBetweenTicks){
+        timeToLastTick = new Date();
+        doGameTick();
+    }
+    setTimeout(()=>{
+        startLoop(); // calls it self in the end to determine the time.
+    },5);
+    
+    
+}
 
 module.exports = { 
     init : function() {
+        
+        console.log(`  -EN> Starting Game Engine Tick Time:${timeBetweenTicks}ms`);
+        timeToLastTick = new Date(); // this is what starts thecount;
+        doGameTick();
+        startLoop();
     },
  };
--- server/server.js
+++ server/server.js
@@ -1,41 +1,37 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 //const { authMiddleware } = require('./utils/auth');
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 
-//const {Engine} = require('./engine/');
+const {Engine} = require('./engine/');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Socket.io Stuff 
-const {initIo} = require('./socket/index'); // initIo to initalize the server, io later on just to grab the object.
+// Socket.io Stuff
+const { initIo } = require("./socket/index"); // initIo to initalize the server, io later on just to grab the object.
 const ioServer = initIo(app); // initalizing io into serverIo
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  ioServer.listen(PORT, () => {
+    // IO port being opened.
+
+    console.log(`  -EX> Express server running on port ${PORT}!`);
+    console.log(`  -IO> Socket.io listening on http://localHost:${PORT}?`);
+    Engine.init();
+  });
 });
 
 
-
-
-  db.once('open', () => {
-    ioServer.listen(PORT, () => { // IO port being opened.
-      
-      console.log(`  -API> API server running on port ${PORT}!`);
-      console.log(`  -IO> Socket.io listening on http://localHost:${PORT}?`);
-    });
-  })
-  
-  
-  // Engine.init();
-  
