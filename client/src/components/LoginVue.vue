3944cff50a0e6d395eca2fb1ce07b6be2a67f26c
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-10 12:15:32 -0700

    login w/ form

--- client/src/components/LoginVue.vue
+++ client/src/components/LoginVue.vue
@@ -4,10 +4,10 @@
             <h1>Lanista</h1>
         </div>
         <div class="flex flex-col text-sm rounded-md">
-          <input class="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-yellow-500 " type="text" placeholder="Username or Email id" />
-          <input class="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-yellow-500" type="password" placeholder="Password" />
+          <input class="mb-5 rounded-[4px] border p-3 hover:outline-none focus:outline-none hover:border-yellow-500 " type="text" placeholder="Username or Email id" v-model="username"/>
+          <input class="border rounded-[4px] p-3 hover:outline-none focus:outline-none hover:border-yellow-500" type="password" placeholder="Password" v-model="password" />
         </div>
-        <button class="mt-5 w-full border p-2 bg-gradient-to-r from-gray-800 bg-gray-500 text-white rounded-[4px] hover:bg-slate-400 scale-105 duration-300" type="submit">Sign in</button>
+        <button @click="tryLogin" class="mt-5 w-full border p-2 bg-gradient-to-r from-gray-800 bg-gray-500 text-white rounded-[4px] hover:bg-slate-400 scale-105 duration-300" type="submit">Sign in</button>
         <div class="mt-5 flex justify-between text-sm text-gray-600">
           <a href="#">Forgot password?</a>
           <a href="#">Sign up</a>
@@ -17,7 +17,24 @@
 
 <script>
     export default {
-        name:"LoginVue"
+        name:"LoginVue",
+        data(){
+          return {
+            username:"",
+            password:""
+          }
+        },
+        methods:{
+          async tryLogin(){
+            console.log({username: this.username, password:this.password});
+            console.log( process.env );
+            const user = await fetch("/users/login",{
+              method: 'POST', 
+              body: JSON.stringify({username: this.username, password:this.password})
+            });
+            console.log(user);
+          }
+        }
     }
 </script>
 
