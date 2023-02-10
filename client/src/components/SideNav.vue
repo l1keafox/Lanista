af39e8b4fae7d2e4599ee0e65da051ac16f73a1c
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-09 17:47:06 -0700

    updated butons

--- client/src/components/SideNav.vue
+++ client/src/components/SideNav.vue
@@ -1,39 +1,39 @@
 <template>
 	<div class="overflow-y-auto max-h-screen bg-blue-900 px-3 w-48">
 		<div
-			class=" font-lux text-base m-2 cursor-pointer sideOptions rounded"
+			class="  font-lux text-base m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 			@click="$emit('changeMain', 'WelcomeMain')">
 			Home
 		</div>
 
 		<div v-if="isLoggedIn" class="font-lux text-base">
 			<div
-				class="m-2 cursor-pointer sideOptions rounded"
+				class="m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 				@click="$emit('changeMain', 'GladiatorsMain')">
 				Gladiators
 			</div>
 			<div
-				class="m-2 cursor-pointer sideOptions rounded"
+				class="m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 				@click="$emit('changeMain', 'SchoolMain')">
 				School
 			</div>
 			<div
-				class="m-2 cursor-pointer sideOptions rounded"
+				class="m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 				@click="$emit('changeMain', 'CombatMain')">
 				Combat
 			</div>
 			<div
-				class="m-2 cursor-pointer sideOptions rounded"
+				class="m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 				@click="$emit('changeMain', 'StoreMain')">
 				Store
 			</div>
 			<div
-				class="m-2 cursor-pointer sideOptions rounded"
+				class="m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 				@click="$emit('changeMain', 'StudentMain')">
 				Students
 			</div>
 			<div
-				class="m-2 cursor-pointer sideOptions rounded"
+				class="m-2 cursor-pointer sideOptions rounded hover:bg-blue-200 hover:text-black"
 				@click="$emit('changeMain', 'TournamentMain')">
 				Tournament
 			</div>
@@ -77,8 +77,8 @@ export default {
 	opacity: 1;
 }
 .sideOptions {
-	background-color: rgb(200, 200, 200);
-	color: black;
+	
+	color: rgb(200, 200, 200);
 	padding: 1rem;
 }
 </style>
