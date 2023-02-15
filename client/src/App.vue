ebf26690dc5e25360a951179ef66662d2d5db93f
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-15 11:06:04 -0700

    just cutting til it works

--- client/src/App.vue
+++ client/src/App.vue
@@ -63,16 +63,16 @@ export default {
 			if (this.isLoggedIn) {
         
         try{
-          let port = process.env.PORT ? process.env.PORT :  3001;
-          const rpnse = await fetch(
-					`http://${window.location.hostname}:${port}/owner/${auth.getUser().ownerId}`,
-					{
-						headers: { "Content-Type": "application/json" },
-					}
-				);
-        const oData = await rpnse.json();
-				this.ownerData = oData.owner;
-        this.timeData = oData.time;
+        //   let port = process.env.PORT ? process.env.PORT :  3001;
+        //   const rpnse = await fetch(
+				// 	`http://${window.location.hostname}:${port}/owner/${auth.getUser().ownerId}`,
+				// 	{
+				// 		headers: { "Content-Type": "application/json" },
+				// 	}
+				// );
+        // const oData = await rpnse.json();
+				// this.ownerData = oData.owner;
+        // this.timeData = oData.time;
 
         }catch(err){
           console.log(err, "clearing")
