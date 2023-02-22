22a4937628f495b5e00a5adbe91ce8865cc0b3e4
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-22 16:22:26 -0700

    cleaned up min size on item cards, and fixed store buying issue

--- client/src/components/ItemCard.vue
+++ client/src/components/ItemCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-64 w-64 p-3 m-3 cursor-default select-none flex flex-col bg-slate-700 rounded-lg" v-if="data">
+    <div class="h-64 w-[12rem] min-w-[12rem] p-3 m-3 cursor-default select-none flex flex-col bg-slate-700 rounded-lg" v-if="data">
         <h1 :class="cardTitle">{{ data.type }}</h1>
         <hr />
         <div class = "h-full flex flex-col justify-between">
