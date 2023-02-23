f203069b0d954ebdb270aa8444248bb7466a29eb
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-22 22:09:44 -0700

    added json

--- client/src/components/Character.vue
+++ client/src/components/Character.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import json from './../assets/animation-data/hume1.json'
+import json from './../assets/animation-data/hume2.json'
 import spritesheet from './../assets/hume1.png'
 import { useIntervalFn } from '@vueuse/core'
 import {ref} from 'vue'
@@ -23,20 +23,20 @@ export default {
     const index = ref(0)
     const activePosition = ref("")
     const { pause, resume, isActive } = useIntervalFn(() => {
-      index.value++;
-      // let frame = json[index.value];
-      if(index.value >= 7){
-        index.value = 1;        
+      if(index.value >= 6){
+        index.value = 0;        
       }
- 
-      let frame = json.find(entry=>entry.name == "left"+index.value)
-      //
+      let frame = json.find(entry=>entry.name == "walkRight"+index.value)
+      if(frame)
       activePosition.value = `
+
         background-position-x: -${frame.x}px; 
         background-position-y: -${frame.y}px;
         width: ${frame.width}px;
         height: ${frame.height}px;
-      `        
+      `      
+      index.value++
+
     }, 135)
 
     return {
@@ -51,20 +51,9 @@ export default {
 .sprite {
   /* display the image*/
   background: url("./../assets/hume1.png") no-repeat;
-  /* each frame is 75px wide so limit container to display one at a time */
-  width: 15px; 
-
-  /* main is roughly 150px tall */
-  height: 30px;
-
-  /* the image has some space on top and bottom so this accounts for that 
-    So next frame would be at 50 x 
-    // -25
-    // -87 
-    35 is the Y axis
-  */
-   
+  width: 14px; 
+  height: 29px;
   background-position-x: -25px ;
-  background-position-y: -462px ;
+  background-position-y: -399px ;
 }
 </style>
