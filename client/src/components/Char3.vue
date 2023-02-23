ee1f2ca227c58f9f10a2ae27c8bcfb0aa65bf173
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-22 21:12:50 -0700

    movement animation

--- client/src/components/Char3.vue
+++ client/src/components/Char3.vue
@@ -24,38 +24,53 @@ export default {
     const activePosition = ref("")
     const { pause, resume, isActive } = useIntervalFn(() => {
       index.value++;
-      // let frame = json[index.value];
-      if(index.value >= 7){
-        index.value = 1;        
-      }
- 
-      let frame = json.find(entry=>entry.name == "left"+index.value)
-      //
-      activePosition.value = `
+       console.log(index.value)
+       //,json[this.index]
+       let frame = json[index.value];
+       activePosition.value = `
         background-position-x: -${frame.x}px; 
         background-position-y: -${frame.y}px;
         width: ${frame.width}px;
         height: ${frame.height}px;
       `        
-    }, 135)
+    }, 250)
 
     return {
       index,
       activePosition
     }
   },
-
+  mounted(){
+    // setInterval(() => {
+    //   this.index++;
+    //   console.log(this.index,json[this.index])
+    //   this.activePosition = `
+    //     background-position-x: -${this.frame.x}px; 
+    //     background-position-y: -${this.frame.y}px;
+    //     width: ${this.frame.width}px;
+    //     height: ${this.frame.height}px;
+    //   ` 
+    // }, 1000);
+    this.frame = json[this.index];
+    this.activePosition = `
+    background-position-x: -${this.frame.x}px; 
+    background-position-y: -${this.frame.y}px;
+    width: ${this.frame.width}px;
+    height: ${this.frame.height}px;
+    ` 
+  },
 }
 </script>
 <style scoped>
 .sprite {
   /* display the image*/
   background: url("./../assets/hume1.png") no-repeat;
+
   /* each frame is 75px wide so limit container to display one at a time */
-  width: 15px; 
+  width: 16px; 
 
   /* main is roughly 150px tall */
-  height: 30px;
+  height: 32px;
 
   /* the image has some space on top and bottom so this accounts for that 
     So next frame would be at 50 x 
@@ -63,8 +78,5 @@ export default {
     // -87 
     35 is the Y axis
   */
-   
-  background-position-x: -25px ;
-  background-position-y: -462px ;
 }
 </style>
