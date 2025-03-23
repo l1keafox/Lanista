7feaca0363cfac4cd60792a61a3b5b7d515839ae
l1keafox <raymond.ed.lewis@gmail.com>
2025-03-23 16:06:28 -0600

    fe

--- client/src/App.vue
+++ client/src/App.vue
@@ -63,11 +63,11 @@ let timeTimer;
 let countDown;
 let interval;
 let timerInterval;
-console.log(`Backend API string : ${location.protocol}://${import.meta.env.VITE_BACKEND_STRING}:${import.meta.env.VITE_PORT}`);
+console.log(`Backend API string : ${location.protocol}://${window.location.hostname}:${import.meta.env.VITE_PORT ?? 3006}`);
 const apiData =
 	location.protocol === "https:"
 		? `https://${window.location.hostname}`
-		: `http://${import.meta.env.VITE_BACKEND_STRING ?? window.location.hostname}:${import.meta.env.VITE_PORT ?? 3001}`;
+		: `http://${window.location.hostname}:${3006}`;
 const isLoggedIn = ref(auth.loggedIn());
 const toolTipType = ref(null);
 const showTutorialModal = ref(false);
